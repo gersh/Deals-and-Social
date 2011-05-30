b9815af116a7f7c72b53288d67ea7b122ab4f2f7
Tony Jiang <yujiang99@gmail.com>
2011-05-29 23:05:59 -0500

    cleaning up and improving posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(:text => params[:post][:text],
-    :theme_id => params[:post][:theme_id],
-    :twitter_user => params[:post][:twitter_user])
+    #@post = Post.new(:text => params[:post][:text],
+    #:theme_id => params[:post][:theme_id],
+    #:twitter_user => params[:post][:twitter_user])
+    @post = Post.new(params[:post])
     @post.save
     thid = @post.theme_id
     redirect_to :controller => :glue, :action => :show, :id => thid
