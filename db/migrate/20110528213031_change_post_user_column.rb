b9f36182cc03b42879a48968f9c43e0a7e047021
Tony Jiang <yujiang99@gmail.com>
2011-06-03 13:12:39 -0500

    cleaning up deleted files

--- db/migrate/20110528213031_change_post_user_column.rb
+++
@@ -1,9 +0,0 @@
-class ChangePostUserColumn < ActiveRecord::Migration
-  def self.up
-    drop_column :posts, :twitter_user
-  end
-
-  def self.down
-    add_column :posts, :twitter_user, :string
-  end
-end
